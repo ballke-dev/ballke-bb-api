0a8b3a2cf8885e73a4cac35e5b252227079a274b
mascDriver <diogoblibi@hotmail.com>
2021-02-11 17:34:34 -0300

    renderização de boleto em formato pdf carteira 17 and add timeout

--- apiBB/layout/pdf.py
+++ apiBB/layout/pdf.py
@@ -861,6 +861,6 @@ class BoletoPDF(object):
 
 def load_image(logo_image):
     """Load Bank Image"""
-    pyboleto_dir = os.path.abspath(__file__)
+    pyboleto_dir = os.path.dirname(os.path.abspath(__file__))
     image_path = os.path.join(pyboleto_dir, 'assets/img/', logo_image)
     return image_path
